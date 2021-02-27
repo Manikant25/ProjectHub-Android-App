e21591c55f82ffd81ca4c951b95d39b8dd41df30
Manikant Rai <manikant.rai2000@gmail.com>
2021-02-28 00:11:18 +0530

    splashScreen added

--- app/src/main/java/com/mani/projecthub/splashActivity.kt
+++ app/src/main/java/com/mani/projecthub/splashActivity.kt
@@ -2,10 +2,17 @@ package com.mani.projecthub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 
 class splashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
     }
 }
