5766c56a5b7dcb0c451a02b658469c68096ac5e1
Manikant Rai <manikant.rai2000@gmail.com>
2021-02-28 13:12:48 +0530

    data binding enabled

--- app/src/main/java/com/mani/projecthub/signUpActivity.kt
+++ app/src/main/java/com/mani/projecthub/signUpActivity.kt
@@ -2,10 +2,18 @@ package com.mani.projecthub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mani.projecthub.databinding.ActivitySignUpBinding
 
 class signUpActivity : AppCompatActivity() {
+    private lateinit var binding:ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    private fun setupActionBar(){
+        setSupportActionBar(binding.toolbarSignUpActivity)
     }
 }
