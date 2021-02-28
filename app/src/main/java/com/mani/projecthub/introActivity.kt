98c68cf985d8ed702b15dfebfbbd0b2457e6c3e6
Manikant Rai <manikant.rai2000@gmail.com>
2021-02-28 12:57:49 +0530

    sign up added

--- app/src/main/java/com/mani/projecthub/introActivity.kt
+++ app/src/main/java/com/mani/projecthub/introActivity.kt
@@ -1,11 +1,21 @@
 package com.mani.projecthub
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
 
 class introActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
     }
+
 }
--- app/src/main/java/com/mani/projecthub/splashActivity.kt
+++ app/src/main/java/com/mani/projecthub/splashActivity.kt
@@ -1,18 +1,34 @@
 package com.mani.projecthub
 
+import android.content.Intent
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.Window
 import android.view.WindowManager
+import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledExecutorService
+import java.util.concurrent.TimeUnit
 
 class splashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
+        val backgroundExecutor: ScheduledExecutorService = Executors.newSingleThreadScheduledExecutor()
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
+//        Handler().postDelayed({ #Depricated
+//            startActivity(Intent(this,introActivity::class.java))
+//            finish()
+//        },2500)
+        // Execute a task in the background thread after 3 seconds.
+        backgroundExecutor.schedule({
+            // Your code logic goes here
+            startActivity(Intent(this,introActivity::class.java))
+            finish()
+        }, 3, TimeUnit.SECONDS)
     }
 }
